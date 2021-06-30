72fb0d505b9137f86ff9b84e0a97bd70308f2745
muslim5323 <82503904+muslim5323@users.noreply.github.com>
2021-06-30 10:02:25 +0300

    Add files via upload

---
+++ portfolio_3/pracrice js/Decision/main.js
@@ -0,0 +1,73 @@
+let correctAnswers = 0
+
+let some = prompt('Решите задачу! 2345*234')
+if (some ==548730){
+    alert('Вы правильно решили')
+    correctAnswers ++
+}
+else alert('Вы не правильно решили')
+
+
+let some1 = prompt('Решите задачу! 342*56')
+if (some1 ==19152){
+    alert('Вы правильно решили')
+    correctAnswers ++
+}
+else alert('Вы не правильно решили')
+
+
+let some2 = prompt('Решите задачу! 12890*456')
+if (some2 == 5877840){
+    alert('Вы правильно решили')
+    correctAnswers ++
+}
+else alert('Вы не правильно решили')
+
+
+
+let some3 = prompt('Решите задачу! 99*23')
+if (some3 == 2277){
+    alert('Вы правильно решили')
+    correctAnswers ++
+}
+else alert('Вы правильно решили')
+
+
+
+let some4 = prompt('Решите задачу! 7789*5')
+if (some4 ==38945){
+    alert('Вы правильно решили')
+    correctAnswers ++
+}
+else alert('Вы не првильно решили')
+
+
+let some5 = prompt('Решите задачу! 3451*9')
+if (some5 ==31059){
+    alert('Вы правильно решили ')
+    correctAnswers ++
+}
+else alert('Вы не правильно решили')
+
+
+
+let some6 = prompt('Решите задачу! 567*34')
+if (some6 ==19278){
+    alert('Вы правильно решили')
+    correctAnswers ++
+}
+else alert('Вы не правильно решили')
+
+
+
+
+
+let some7 = prompt('Решите задачу! 78*9')
+if (some7 ==702){
+    alert('Вы правильно решили')
+    correctAnswers ++
+}
+else alert('Вы не правильно решили')
+
+
+let some19 = alert ('Вы ответили ' + correctAnswers + ' вопросов правилно из 9')
